7f481a5ebcc5fcb55115754c51cc0e6511ebf868
zsma <zsma@fresnou.org>
2025-01-29 09:23:54 -0800

    Initial Commit (continue to slide 28, check errors))

--- controllers/restaurantControllers.js
+++ controllers/restaurantControllers.js
@@ -1,5 +1,31 @@
 const {Restaurant} = require('../models');
+const categories =  ['Italian', 'Fast Food'];
+
 module.exports.viewAll = async function(req, res) {
     const restaurants = await restaurants.findAll();
     res.render('index', {restaurants});
+}
+
+module.exports.renderEditForm = async function(req, res) {
+    const restaurant = await Restaurant.findByPk(
+        req.params.id
+    );
+    res.render('edit', {restaurant, categories});
+}
+
+module.exports.updateRestaurant = async function(req, res) {
+    await Restaurant.update(
+        {
+            name: req.body.name,
+            category: req.body.category,
+            rating: req.body.rating,
+            image: req.body.image,
+            description: req.body.description
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        });
+    res.redirect('/')
 }
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,7 @@ const restaurantController = require('../controllers/restaurantControllers');
 
 /* GET home page. */
 router.get('/', restaurantController.viewAll);
+router.get('/edit/:id', restaurantController.renderEditForm);
 
 module.exports = router;
+
